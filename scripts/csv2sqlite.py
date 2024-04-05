b6de4cd5b8c939e38de2ee488e41db26e6bb78b8
flambuth <flambuth@gmail.com>
2024-04-05 09:10:52 -0500

    adding global scripts to keep global dash fresh

---
+++ scripts/csv2sqlite.py
@@ -0,0 +1,10 @@
+import json
+from app.dash_plotlys.global_stats import initiate_db
+
+with open("country_codes.json", "r") as json_file:
+    country_codes = json.load(json_file)
+
+country_names = list(country_codes.values())
+
+initiate_db(country_names)
+print(f"You wrote something down to be printed at the last line of csv2sqlite")
