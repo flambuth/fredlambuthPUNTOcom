6489a77cf4913da5d8cb9a08dfa10e9f9bb202f5
flambuth <flambuth@gmail.com>
2024-09-22 16:39:54 -0500

    fixing titles on plotly figues

--- scripts/copy_prod_db_to_dev.sh
+++ scripts/copy_prod_db_to_dev.sh
@@ -5,3 +5,4 @@ ARCHIVE_DIR="/home/flambuth/archives"
 
 cd "$PROJECT_DIR"
 scp "flambuth@flask_site:$PROJECT_DIR/data/fred.db" "$ARCHIVE_DIR/fred_dbs/"
+scp "flambuth@flask_site:$PROJECT_DIR/data/global.db" "$ARCHIVE_DIR/global/"
